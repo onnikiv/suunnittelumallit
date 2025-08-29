fa345354287c5dcbef1c51c43afa8536b9d3c38d
onnik <onni.kivinen@gmail.com>
2025-08-29 17:25:52 +0300

    Finishing assignment Decorator. Implemented a dummy encryption to EncryptedPrinter class.

--- src/main/java/decorator/EncryptedPrinter.java
+++ src/main/java/decorator/EncryptedPrinter.java
@@ -2,12 +2,28 @@ package decorator;
 
 public class EncryptedPrinter extends PrinterDecorator {
 
+    private final String VOWELS = "aeiouAEIOU";
+
     public EncryptedPrinter(Printer printer) {
         super(printer);
     }
 
     @Override
     public void print(String message) {
-        super.print(")(/FG#¤%DGGFJSREWfd)");
+
+        String encryptedMessage = "";
+
+        for (int i = 0; i < message.length(); i++) {
+            char currentChar = message.charAt(i);
+            if (VOWELS.contains(String.valueOf(currentChar))) {
+                // jos vokaali 
+                encryptedMessage += "*";
+            } else {
+                // jos konsonatti
+                encryptedMessage += "#";
+            }
+        }
+
+        super.print(encryptedMessage);
     }
 }
