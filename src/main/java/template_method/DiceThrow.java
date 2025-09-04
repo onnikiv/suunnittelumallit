33ee624030562cb9e9fb8704dbaaee686757fc55
onnik <onni.kivinen@gmail.com>
2025-09-04 13:44:28 +0300

    lisätty mausteita yatziin

--- src/main/java/template_method/DiceThrow.java
+++ src/main/java/template_method/DiceThrow.java
@@ -8,14 +8,32 @@ public class DiceThrow {
     private final ArrayList<Dice> diceThrow;
     private static final Scanner scanner = new Scanner(System.in);
 
+    private int sum;
+
+    @SuppressWarnings("OverridableMethodCallInConstructor")
     public DiceThrow() {
+        this.sum = 0;
         this.diceThrow = new ArrayList<>();
 
+        initializeDices();
+    }
+
+    public void initializeDices() {
         for (int i = 0; i < 5; i++) {
             this.diceThrow.add(new Dice());
         }
+    }
 
-        displayDice();
+    public void printSum() {
+        System.out.println(" ".repeat(70) + "| SUMMA  : " + getSum() + " |");
+    }
+
+    public int getSum() {
+        this.sum = 0;
+        for (Dice dice : diceThrow) {
+            this.sum += dice.getFace();
+        }
+        return this.sum;
     }
 
     public void throwNew(int dice) {
@@ -25,14 +43,28 @@ public class DiceThrow {
         }
     }
 
+    public void testPlay() {
+        System.out.println();
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(" ".repeat(70) + "| HEITTO : " + i + ". |");
+            printSum();
+            displayDice();
+            diceToThrowAgain();
+        }
+        System.out.println("Tämän vuoron summa: ");
+        printSum();
+    }
+
     public void diceToThrowAgain() {
         ArrayList<Integer> diceToThrow = new ArrayList<>();
         System.out.println("Select dices to throw again (1-5):    Enter (0) to quit.");
 
         while (true) {
+
             int dice = scanner.nextInt();
 
             if (dice == 0) {
+
                 break;
             }
 
@@ -57,7 +89,6 @@ public class DiceThrow {
             throwNew(diceToThrow.get(i));
         }
 
-        displayDice();
     }
 
     public void displayDice() {
