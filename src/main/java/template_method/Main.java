01a5b7b0e767872f29ae78548b7671010f665acb
onnik <onni.kivinen@gmail.com>
2025-09-06 13:32:22 +0300

    finishing template_method assignment.

--- src/main/java/template_method/Main.java
+++ src/main/java/template_method/Main.java
@@ -2,6 +2,14 @@ package template_method;
 
 public class Main {
 
+    /**
+     * Köyhän miehen Yatzee peli. Pelataan 5 roundia. Voittaja on se joka on
+     * summannut eniten pisteitä (noppien silmälukujen summia) pelin lopuksi.
+     *
+     * Todo: tulevaisuudessa mahdollisesti ohjelmoida peliin loput
+     * toiminnallisuudet. Mutta tähän tehtävään tullut käytettyä muutenkin vähän
+     * liikaa aikaa, joten aika siirtyä seuraavaan.
+     */
     public static void main(String[] args) {
         Yatzee yatzee = new Yatzee();
         yatzee.play(2);
--- src/main/java/template_method/Yatzee.java
+++ src/main/java/template_method/Yatzee.java
@@ -7,6 +7,8 @@ public class Yatzee extends Game {
 
     private ArrayList<Player> players;
     private HashMap<Integer, Integer> scores;
+    private int round = 0;
+    private static final int MAX_ROUNDS = 5; // otetaan vaan 5 roundii
 
     @Override
     public void initializeGame(int numberOfPlayers) {
@@ -19,12 +21,13 @@ public class Yatzee extends Game {
             this.players.add(new Player(i));
             this.scores.put(i, 0);
         }
+        this.round = 0;
 
     }
 
     @Override
     public boolean endOfGame() {
-        return false;
+        return this.round >= MAX_ROUNDS * this.players.size();
     }
 
     @Override
@@ -39,11 +42,22 @@ public class Yatzee extends Game {
         int turnScore = playerInTurn.getTheSumOfTheDices();
         System.out.println("PELAAJA " + (playerNumber + 1) + ". " + "PISTEET TÄLTÄ VUOROLTA: " + turnScore);
         this.scores.put(playerNumber, currentScore + turnScore);
+        this.round++;
 
     }
 
     @Override
     public void displayWinner() {
-
+        int winner = -1;
+        int maxScore = Integer.MIN_VALUE;
+        for (int player : scores.keySet()) {
+            int score = scores.get(player);
+            System.out.println("PELAAJA " + (player + 1) + ": " + score + " pistettä");
+            if (score > maxScore) {
+                maxScore = score;
+                winner = player;
+            }
+        }
+        System.out.println("\nVOITTAJA ON PELAAJA " + (winner + 1) + "! PISTEET: " + maxScore);
     }
 }
